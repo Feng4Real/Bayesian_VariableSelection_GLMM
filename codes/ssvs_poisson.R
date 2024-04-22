d7b4e190fe9fc52d145c26d0d6d0858e6b5c4da0
Feng4Real <155674660+Feng4Real@users.noreply.github.com>
2024-04-22 00:50:58 -0700

    Update ssvs_poisson.R

--- codes/ssvs_poisson.R
+++ codes/ssvs_poisson.R
@@ -23,18 +23,18 @@ sample_generate = function()
   #cov_mat = matrix(c(1,0,0,0,0,0.9,0.48,0.06,0,0.48,0.4,0.1,0,0.06,0.1,0.1 ),ncol=4)
   Q_mu = c(0,0,0,0)
   samples = matrix(nrow = 1000, ncol = 10)
-  # yij = Xij^T beta + Zij^T Q + epslion
-  
   Qi = matrix(NA, nrow = n, ncol = 4)
   index = 1
-  for (i in 1:n) { ## i is group id
-    Qi[i,] = mvrnorm(1, Q_mu, cov_mat) ## Qi are random effects for group i
-    print(Qi[i,])
+  
+  ## i is group id
+  for (i in 1:n) {
+    ## Qi are random effects for group i
+    Qi[i,] = mvrnorm(1, Q_mu, cov_mat) 
+
     for (j in 1:n_i) {
       Xij = c(1,runif(7,-1,1))
       Zij = Xij[1:4]
-      #epsij = rnorm(n=1,mean=0,sd=sigma)
-      yij = rpois(1,exp(Xij%*%beta+Zij %*% Qi[i,]))
+      yij = rpois(1, exp(Xij%*%beta+Zij %*% Qi[i,]))
       samples[index,1] = i
       samples[index,2:9] = Xij
       samples[index, 10] = yij
@@ -44,11 +44,16 @@ sample_generate = function()
   colnames(samples) = c("id","X1","X2","X3","X4","X5","X6","X7","X8","Y")
   return(list(samples = samples, re = Qi))
 }
+
+
 samples = sample_generate()
 Y = samples$samples[,10]
 X= samples$samples[,2:9]
 ## Confirm different relationships
 samples.df = data.frame(samples$samples)
+
+
+# double pareto model for the fixed effects
 
 double_pareto = "model {
   # priors for r
@@ -123,7 +128,6 @@ double_pareto = "model {
 
   # likelihood
   for (i in 1:nobs) {
-    #Y[i] ~ dnorm(X[i, ] %*% beta[]+ X[i,1:4] %*% Lambda_mat %*% Gamma_mat %*%  Xi[index[i], ], 2) # We can estimate the variance
     Y[i] ~ dpois( exp(X[i, ] %*% beta[]+ X[i,1:4] %*% Lambda_mat %*% Gamma_mat %*%  Xi[index[i], ]) )
   }
 }
@@ -187,8 +191,6 @@ horse_shoe = "model {
   for (i in 1:l){
     nu2[i] ~ dt(0,1,1) T(0,) # cauchy is special T
     beta_K_k[i] ~ dt(0,nu2[i],1) T(0,) # cauchy is special T
-    
-
     beta_side1[i] ~ dnorm(0, sigma2/(g*beta_K_k[i]) ) 
     beta_side2[i] = 0
     latent_J[i] ~ dbern(0.5)
@@ -199,31 +201,49 @@ horse_shoe = "model {
 
   # likelihood
   for (i in 1:nobs) {
-    #Y[i] ~ dnorm(X[i, ] %*% beta[]+ X[i,1:4] %*% Lambda_mat %*% Gamma_mat %*%  Xi[index[i], ], 2) # We can estimate the variance
     Y[i] ~ dpois( exp(X[i, ] %*% beta[]+ X[i,1:4] %*% Lambda_mat %*% Gamma_mat %*%  Xi[index[i], ]) )
   }
 }
 "
-
-cat(horse_shoe, file="ssvs.txt")
+# prior for lower-triangular random effect matrix
 cov_mat_R = diag(1,nrow = 6)
 
-#dat <- list(p=4, l=8, nobs=1000, n = 100, cov_mat= cov_mat_R, X=X, Y=Y, h= 0.1, v=0.01, nu=0.01,
-#            index = samples.df$id)
-dat <- list(p=4, l=8, nobs=1000, n = 100, cov_mat= cov_mat_R, X=X, Y=Y, h= 0.1, index=samples.df$id)
+cat(horse_shoe, file="horse_shoe_ssvs.txt")
+cat(double_pareto, file="double_pareto_ssvs.txt")
+
+double_pareto_dat <- list(p=4, l=8, nobs=1000, n = 100, cov_mat= cov_mat_R, X=X, Y=Y, h= 0.1, v=0.01, nu=0.01,
+            index = samples.df$id)
+horse_shoe_dat <- list(p=4, l=8, nobs=1000, n = 100, cov_mat= cov_mat_R, X=X, Y=Y, h= 0.1, index=samples.df$id)
 vars <- c("beta", "Gamma_mat", "Lambda_mat", "Xi", "g", "sigma2", "beta_K_k")
-out <- run.jags("ssvs.txt", vars, data=dat, n.chains=3,
+horse_shoe_out <- run.jags("horse_shoe_ssvs.txt", vars, data=horse_shoe_dat, n.chains=3,
                 adapt=100, burnin=300, sample=100)
-summary(out) 
-
-par.est = data.frame(as.mcmc(out))
-plot(par.est$beta.1.)
-plot(par.est$beta.2.)
-plot(par.est$beta.3.)
-plot(par.est$beta.4.)
-plot(par.est$beta.5.)
+double_pareto_out <- run.jags("double_pareto_ssvs.txt", vars, data=double_pareto_dat, n.chains=3,
+                              adapt=100, burnin=300, sample=100)
 
 
+summary(horse_shoe_out) 
+summary(double_pareto_out)
+
+horse_shoe_par.est = data.frame(as.mcmc(horse_shoe_out))
+double_pareto_par.est = data.frame(as.mcmc(double_pareto_out))
+
+plot(horse_shoe_par.est$beta.1.)
+plot(horse_shoe_par.est$beta.2.)
+plot(horse_shoe_par.est$beta.3.)
+plot(horse_shoe_par.est$beta.4.)
+plot(horse_shoe_par.est$beta.5.)
+plot(horse_shoe_par.est$beta.6.)
+plot(horse_shoe_par.est$beta.7.)
+plot(horse_shoe_par.est$beta.8.)
+
+# the random effects are not being shrinking to 0,
+# this means we are over-selecting?
+
+horse_shoe_Lambda_mat = matrix(colMeans( horse_shoe_par.est[,25:40] ), ncol = 4, nrow =4 )
+horse_shoe_Lambda_mat
+
+double_pareto_Lambda_mat = matrix(colMeans( double_pareto_par.est[,25:40] ), ncol = 4, nrow =4 )
+double_pareto_Lambda_mat
 
 
 
@@ -233,48 +253,37 @@ names(par.est)[25]
 names(par.est)[40]
 
 ## see if we can reconstruct the true covariance matrix
-Xi = matrix(colMeans( par.est[,41:440] ), ncol = 4, nrow =100 )
-Gamma_mat = matrix(colMeans( par.est[,9:24] ), ncol = 4, nrow =4 )
-Lambda_mat = matrix(colMeans( par.est[,25:40] ), ncol = 4, nrow =4 )
-par.est
-Gamma_mat
-Lambda_mat
-
-round(Lambda_mat %*% Gamma_mat %*% t(Gamma_mat) %*% (Lambda_mat),4)
-
-true_cov = matrix(c(0,0,0,0,0,0.9,0.48,0.06,0,0.48,0.4,0.1,0,0.06,0.1,0.1 ),ncol=4)
-chol(true_cov, pivot = T) # # chol decomp
-decomp_mat = matrix(chol(true_cov,pivot = T)[1:16],nrow = 4)
-t(decomp_mat) %*% decomp_mat
-t(decomp_mat)
-Lambda_mat %*% Gamma_mat
-
-
-
-
-plot(par.est$g)
-plot(par.est$sigma2)
-
-
-
-
-plot(par.est$Lambda_mat.1.1.)
-plot(par.est$Lambda_mat.2.2.)
-plot(par.est$Lambda_mat.3.3.)
-plot(par.est$Lambda_mat.4.4.)
-
-
-for(i in 1:nrow(par.est)){
-  lambda.mat.tmp = diag(c(par.est$Lambda_mat.1.1.[i],
-                          par.est$Lambda_mat.2.2.[i],
-                          par.est$Lambda_mat.3.3.[i],
-                          par.est$Lambda_mat.4.4.[i]))
-  gamma.mat.tmp = matrix(par.est[i,9:24], ncol = 4)
-}
-
-hist(rgamma(1000,0.1,0.1))
-
-
-
-
-
+# Xi = matrix(colMeans( par.est[,41:440] ), ncol = 4, nrow =100 )
+# Gamma_mat = matrix(colMeans( par.est[,9:24] ), ncol = 4, nrow =4 )
+# Lambda_mat = matrix(colMeans( par.est[,25:40] ), ncol = 4, nrow =4 )
+# par.est
+# Gamma_mat
+# Lambda_mat
+# 
+# round(Lambda_mat %*% Gamma_mat %*% t(Gamma_mat) %*% (Lambda_mat),4)
+# 
+# true_cov = matrix(c(0,0,0,0,0,0.9,0.48,0.06,0,0.48,0.4,0.1,0,0.06,0.1,0.1 ),ncol=4)
+# chol(true_cov, pivot = T) # # chol decomp
+# decomp_mat = matrix(chol(true_cov,pivot = T)[1:16],nrow = 4)
+# t(decomp_mat) %*% decomp_mat
+# t(decomp_mat)
+# Lambda_mat %*% Gamma_mat
+# 
+# 
+# 
+# 
+# plot(par.est$g)
+# plot(par.est$sigma2)
+# 
+# 
+# 
+# 
+# plot(par.est$Lambda_mat.1.1.)
+# plot(par.est$Lambda_mat.2.2.)
+# plot(par.est$Lambda_mat.3.3.)
+# plot(par.est$Lambda_mat.4.4.)
+# 
+# 
+# 
+# 
+# hist(rgamma(1000,0.1,0.1))
